c8f281f701c0c739e5a34266aa650e90bd639013
Justine Cocchi <jucocchi@microsoft.com>
2017-06-08 16:57:12 -0700

    added validation of options close #1

--- code/lib/index.js
+++ code/lib/index.js
@@ -12,6 +12,7 @@ var ItemPromptType;
     ItemPromptType[ItemPromptType["confirm"] = 3] = "confirm";
 })(ItemPromptType = exports.ItemPromptType || (exports.ItemPromptType = {}));
 function createLibrary(options) {
+    validateOptions(options);
     initialSearchDialog.register(lib, options);
     refineSearchDialog.register(lib);
     return lib;
@@ -22,5 +23,23 @@ function itemSearchDialog(session) {
     return session.beginDialog('itemSearch:initialSearch');
 }
 exports.itemSearchDialog = itemSearchDialog;
+;
+function validateOptions(options) {
+    if (options.searchParameters.length < 1) {
+        throw "You must pass in at least one search parameter.";
+    }
+    options.searchParameters.forEach(function (p) {
+        if (p.type < 0 || p.type > 3) {
+            throw "You must use a valid ItemPromptType value.";
+        }
+        else if (p.type == 0 && p.choices.length < 1) {
+            throw "All choice types must also have an array of choices.";
+        }
+    });
+    if (!options.searchFunction) {
+        throw "'searchFunction' parameter missing";
+    }
+    return true;
+}
 ;
 //# sourceMappingURL=index.js.map
--- code/src/index.ts
+++ code/src/index.ts
@@ -26,21 +26,45 @@ export interface IItemSearchPromptOptions {
 }
 
 export enum ItemPromptType {
-  choice = 0,
-  text = 1,
-  number = 2,
-  confirm = 3
+  choice,
+  text,
+  number,
+  confirm
 }
 
 export function createLibrary (options: IItemSearchPromptOptions) {
+  validateOptions(options);
+  
   initialSearchDialog.register(lib, options);
   refineSearchDialog.register(lib);
-
-  // TODO: VALIDATE OPTIONS
   
   return lib;
 };
 
 export function itemSearchDialog (session: builder.Session) {  
   return session.beginDialog('itemSearch:initialSearch');
+};
+
+function validateOptions(options: IItemSearchPromptOptions): boolean {
+  // There must be at least one search parameter
+  if(options.searchParameters.length < 1){
+    throw "You must pass in at least one search parameter.";
+  }
+
+  // The type of each parameter must be an int from 0 - 3
+  // If the type is choice, there must also be a list of choices
+  options.searchParameters.forEach(p => {
+    if(p.type < 0 || p.type > 3){
+      throw "You must use a valid ItemPromptType value."
+    } else if (p.type == 0 && p.choices.length < 1){
+      throw "All choice types must also have an array of choices."
+    }
+  })
+
+  // There must be a search function
+  if(!options.searchFunction){
+    throw "'searchFunction' parameter missing"
+  }
+
+  return true;
 };
